0e8a20ccc81f1733351b1c23e4667e17dd2a24c4
kapsali29 <kapsali29@gmail.com>
2020-03-09 16:50:01 +0200

    Some changes on settings

--- settings.py
+++ settings.py
@@ -2,3 +2,10 @@ import os
 
 # DOBIE Settings
 DOBIE_HOST = os.environ.get('DOBIE_HOST', 'localhost')
+
+# RABBITMQ Settings
+RABBITMQ_HOST = os.environ.get('RABBITMQ_HOST', 'localhost')
+RABBITMQ_PORT = os.environ.get('RABBITMQ_PORT', 5672)
+RABBITMQ_VHOST = os.environ.get('RABBITMQ_VHOST', '/')
+RABBITMQ_USER = os.environ.get('RABBITMQ_USER', 'rabbitmq')
+RABBITMQ_PASSWORD = os.environ.get('RABBITMQ_PASSWORD', 'rabbitmq')
