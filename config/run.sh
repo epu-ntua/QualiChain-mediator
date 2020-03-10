ee4429dc9200c6994d365ae8ecccebea4b3b2d94
epu_temp_steve <sskalidakis@epu.ntua.gr>
2020-03-10 18:36:12 +0200

    docker files, run.sh initial, docker-compose initial

---
+++ config/run.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+
+
+echo 'Waiting for RabbitMQ...'
+
+#while ! nc -z ${RABBITMQ_HOST} 5672; do
+#
+#  sleep 0.1
+#
+#done
+echo 'RabbitMQ Initialization completed'
+
+python consume.py
---
+++ consume.py
@@ -0,0 +1,6 @@
+from clients.rabbitmq_client import RabbitMQClient
+
+if __name__ == "__main__":
+    rabbit_mq = RabbitMQClient()
+    rabbit_mq.consumer(queue='hello')
+
